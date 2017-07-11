ed063ddf9dde6768c29a4d5086298360101a4834
Pawel.Wojtaszko <pawelwojtaszko@o2.pl>
2017-07-11 16:30:34 +0200

    Added some menus and created AllLists Layout.

--- app/src/main/java/com/example/teamengineer/myengineer/MainScreen.java
+++ app/src/main/java/com/example/teamengineer/myengineer/MainScreen.java
@@ -3,8 +3,10 @@ package com.example.teamengineer.myengineer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -15,7 +17,7 @@ public class MainScreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle(R.string.menuMainScreen);
-        
+
 
     }
 
@@ -26,6 +28,11 @@ public class MainScreen extends AppCompatActivity {
 
     public void register(View view) {
         Intent intent = new Intent(MainScreen.this,Register.class);
+        startActivity(intent);
+    }
+
+    public void Lists(View view) {
+        Intent intent = new Intent(MainScreen.this,ListsList.class);
         startActivity(intent);
     }
 }
