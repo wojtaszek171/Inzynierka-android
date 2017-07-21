29379195bd9d525aaabfd0f98567f210b0d130bc
Pawel.Wojtaszko <pawelwojtaszko@o2.pl>
2017-07-21 15:48:57 +0200

    Created activities : adding/editing product, add friend, friends list

---
+++ app/src/main/java/com/example/teamengineer/myengineer/Add_Product.java
@@ -0,0 +1,18 @@
+package com.example.teamengineer.myengineer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Add_Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__product);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(R.string.addProduct);
+    }
+}
