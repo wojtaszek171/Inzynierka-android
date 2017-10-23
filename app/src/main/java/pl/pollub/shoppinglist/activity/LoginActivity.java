44dd7756365b9c6f6a251432de621bcc3f7ccefe
Jakub Wójcik <wujek444@gmail.com>
2017-10-23 21:59:03 +0200

    Prosta rejestracja z logowaniem - bez walidacji.

--- app/src/main/java/pl/pollub/shoppinglist/activity/LoginActivity.java
+++ app/src/main/java/pl/pollub/shoppinglist/activity/LoginActivity.java
@@ -1,13 +1,28 @@
 package pl.pollub.shoppinglist.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import pl.pollub.shoppinglist.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView loginTextView;
+    TextView loginPassword;
+    Button loginButton;
+
+    String login, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,16 @@ public class LoginActivity extends AppCompatActivity {
         setTitle(R.string.menuLogInto);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        loginTextView = (TextView) findViewById(R.id.login_login);
+        loginPassword = (TextView) findViewById(R.id.login_password);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                getLoginFormData();
+                attemptUserLogin();
+            }
+        });
     }
 
     @Override
@@ -30,6 +55,32 @@ public class LoginActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 
+    }
+
+    ////////////////////////////////////////////////////////
+    public void getLoginFormData(){
+        login = loginTextView.getText().toString();
+        password = loginPassword.getText().toString();
+    }
+
+    public void attemptUserLogin(){
+        ParseUser.logInInBackground(login, password, new LogInCallback() {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            CharSequence text = "";
+
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Hooray! The user is logged in.
+                    text = login + " - pomyślnie zalogowano!";
+                } else {
+                    // Signup failed. Look at the ParseException to see what happened.
+                    text = e.getMessage();
+                }
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 
 
--- app/src/main/java/pl/pollub/shoppinglist/activity/RegistrationActivity.java
+++ app/src/main/java/pl/pollub/shoppinglist/activity/RegistrationActivity.java
@@ -1,13 +1,34 @@
 package pl.pollub.shoppinglist.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import pl.pollub.shoppinglist.R;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    TextView registerEmail;
+    TextView registerLogin;
+    TextView registerPassword;
+    TextView registerRepeatedPassword;
+    Button registerButton;
+
+    String email;
+    String login;
+    String password;
+    String repeatedPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +39,52 @@ public class RegistrationActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setTitle(R.string.menuRegister);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        registerEmail = (TextView) findViewById(R.id.register_email);
+        registerLogin = (TextView) findViewById(R.id.register_login);
+        registerPassword = (TextView) findViewById(R.id.login_password);
+        registerRepeatedPassword = (TextView) findViewById(R.id.register_repeat_password);
+        registerButton = (Button) findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                fetchRegisterFormData();
+
+                //TODO: walidacja danych
+
+                if(password.equals(repeatedPassword)){
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(login);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        Context context = getApplicationContext();
+                        int duration = Toast.LENGTH_SHORT;
+                        CharSequence text = "";
+
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                // Hooray! Let them use the app now.
+
+                                resetRegistrationForm();
+                                text = "Pomyślnie zarejestrowano!";
+
+                            } else {
+                                // Sign up didn't succeed. Look at the ParseException
+                                // to figure out what went wrong
+                                resetPasswordTextView();
+                                text = e.getMessage();
+
+                            }
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                        }
+                    });
+                }
+
+            }
+        });
     }
 
     @Override
@@ -30,4 +97,24 @@ public class RegistrationActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /////////////////////////////////////////////////
+    private void resetPasswordTextView(){
+        registerPassword.setText("");
+        registerRepeatedPassword.setText("");
+    }
+
+    private void resetRegistrationForm(){
+        resetPasswordTextView();
+        registerEmail.setText("");
+        registerLogin.setText("");
+    }
+
+    private void fetchRegisterFormData(){
+        email = registerEmail.getText().toString();
+        login = registerLogin.getText().toString();
+        password = registerPassword.getText().toString();
+        repeatedPassword = registerRepeatedPassword.getText().toString();
+    }
+
 }
